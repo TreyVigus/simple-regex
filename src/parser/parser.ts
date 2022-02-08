910b0adb59e519df93f3cbfc22904b9c965becaf
Trey Vigus <trey.vigus@sae.org>
2022-02-08 18:25:38 -0500

    return node instead of children

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -24,58 +24,61 @@ export class RegexParser implements Parser {
      */
     public parse(expression: string): ParseNode | null {
         //return the children of startVar if a parse tree exists.
-        const parseChildren = (startVar: string, start: number, end: number): ParseNode[] => {
+        const parseDFS = (startVar: string, start: number, end: number): ParseNode | null => {
             if(start > end) {
-                return [];
+                return null;
             }
 
             const window = expression.slice(start, end+1);
             const replacements = this.grammar.getReplacements(startVar);
             for(const replacement of replacements) {
                 if(window.length === 1) {
-                    //handle terminal replacements e.g. L
+                    //handle terminal replacements e.g. L->f
                     if(window === replacement) {
-                        return [{value: window}];
+                        return {value: startVar, children: [{value: replacement}]}
                     }
                     continue;
                 }
 
-                const [first, last] = [replacement[0], replacement[replacement.length-1]];
                 //handle R->(R), R->[P], O->{A}, etc
+                const [first, last] = [replacement[0], replacement[replacement.length-1]];
                 if(this.grammar.hasTerminal(first) && this.grammar.hasTerminal(last)) {
                     if(window[start] === first && window[end] === last) {
-                        const children = parseChildren(replacement[1], start+1, end-1);
-                        if(children.length) {
-                            return children;
+                        const child = parseDFS(replacement[1], start+1, end-1);
+                        if(child) {
+                            return {value: startVar, children: [child]}
                         }
                     }
                 } else if(this.grammar.hasVariable(replacement)) { //handle single var replacements e.g. P->L
-                    const children = parseChildren(replacement, start, end);
-                    if(children.length) {
-                        return children;
+                    const child = parseDFS(replacement, start, end);
+                    if(child) {
+                        return {value: startVar, children: [child]}
                     }
                 } else if(replacement === 'I,I') { //handle A->I,I
                     const commaIdx = window.indexOf(',');
                     //TODO: what if mulitiple commas? write unit test for this
                     if(commaIdx) {
-                        const children = parseChildren('I', start, commaIdx-1).concat(parseChildren('I', commaIdx+1, end));
-                        if(children.length) {
-                            return children;
+                        const leftChild = parseDFS('I', start, commaIdx-1);
+                        const rightChild = parseDFS('I', commaIdx+1, end);
+                        if(leftChild && rightChild) {
+                            return {value: startVar, children: [leftChild, rightChild]}
                         }
                     }
                 } else { //handle multiple var replacements i.e R->RO
-                    
+                    for(let i = start; i < end; i++) {
+                        const leftChild = parseDFS(replacement[0], start, i);
+                        const rightChild = parseDFS(replacement[1], i+1, end);
+                        if(leftChild && rightChild) {
+                            return {value: startVar, children: [leftChild, rightChild]};
+                        }
+                    }
                 }
             }
 
-            return children;
+            return null;
         }
         
-        const children = parseChildren(this.grammar.startVariable, 0, expression.length-1);
-        if(children.length) {
-            return {value: this.grammar.startVariable, children}
-        }
-        return null;
+        return parseDFS(this.grammar.startVariable, 0, expression.length-1);
     }
 }
 
