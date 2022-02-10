898b73b79bb749870cd36f5e06e651dafb19638a
Trey Vigus <trey.vigus@sae.org>
2022-02-09 19:49:19 -0500

    add more tests

--- src/parser/parser.test.ts
+++ src/parser/parser.test.ts
@@ -29,6 +29,47 @@ Deno.test('h{1,2}b{1,3} should parse', () => {
     asserts.assertExists(root);
 });
 
+Deno.test('x{0,2}bbc{0,1} should parse', () => {
+    const root = parser.parse('x{0,2}bbc{0,1}');
+    asserts.assertExists(root);
+});
+
+Deno.test('[kj]{0,2} should parse', () => {
+    const root = parser.parse('[kj]{0,2}');
+    asserts.assertExists(root);
+});
+
+Deno.test('(z){1,2} should parse', () => {
+    const root = parser.parse('(z){1,2}');
+    asserts.assertExists(root);
+});
+
+Deno.test('((xy){1,2}){2} should parse', () => {
+    const root = parser.parse('((xy){1,2}){2}');
+    asserts.assertExists(root);
+});
+
+Deno.test('(bc){2,3} should parse', () => {
+    const root = parser.parse('(bc){2,3}');
+    asserts.assertExists(root);
+});
+
+Deno.test('(bc){2,3} should parse', () => {
+    const root = parser.parse('(bc){2,3}');
+    asserts.assertExists(root);
+});
+
+Deno.test('(b{2}){1,2} should parse', () => {
+    const root = parser.parse('(b{2}){1,2}');
+    asserts.assertExists(root);
+});
+
+Deno.test('((ht){1,2}(gv){0,2}){2} should parse', () => {
+    const root = parser.parse('((ht){1,2}(gv){0,2}){2}');
+    asserts.assertExists(root);
+});
+
+
 /** The following tests should NOT parse */
 Deno.test('( should NOT parse', () => {
     const root = parser.parse('(');
@@ -67,5 +108,10 @@ Deno.test('{ should NOT parse', () => {
 
 Deno.test('R should NOT parse', () => {
     const root = parser.parse('R');
+    asserts.assertEquals(root, null);
+});
+
+Deno.test('(b{2}){1,2 should NOT parse', () => {
+    const root = parser.parse('(b{2}){1,2');
     asserts.assertEquals(root, null);
 });
